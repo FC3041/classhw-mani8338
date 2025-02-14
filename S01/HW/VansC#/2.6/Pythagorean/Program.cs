f3d8ae82fd17416a7a98fda8e5a6357272d58c81
mani8338 <manib2500@gmail.com>
2025-02-14 00:52:16 -0800

    C#VansM

--- S01/HW/VansC#/2.6/Pythagorean/Program.cs
+++ S01/HW/VansC#/2.6/Pythagorean/Program.cs
@@ -8,8 +8,28 @@ class Program
             Console.WriteLine("a = " + a + ", b = " + b + ", c = " + c);
         }
     }
+
+    public static bool is_these_3_Pythagorean2(int a,int b,int c)
+    {
+        if(a*a + b*b == c*c){
+            return true;
+        }else{
+            return false;
+        }
+    }
     static void Main(string[] args)
     {
+        for(int c = 2; c < 100; c++){
+            for(int b = 1; b < c; b++){
+                for(int a = 1; a < b; a++){
+                    //is_these_3_Pythagorean(a,b,c);
+                    if(is_these_3_Pythagorean2(a,b,c)){
+                        Console.WriteLine("a = " + a + ", b = " + b + ", c = " + c);
+                    }
+                }
+            }
+        }
+        Console.WriteLine();
         for(int c = 2; c < 100; c++){
             for(int b = 1; b < c; b++){
                 for(int a = 1; a < b; a++){
