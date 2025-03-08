843325a0abddc8d408f454e8f9e18508ee94cb5b
mani8338 <manib2500@gmail.com>
2025-03-07 22:46:12 -0800

    error after error

---
+++ S08/Class/s8con.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace s8con.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        int x = 1;
+        Assert.Equal(x+1,2);
+    }
+    public void Test2()
+    {
+        int result = Program.add(3,5);
+        Assert.Equal(result,8);
+    }
+}
