4d36409907714b3a6d20347dabc2d5f68dd65812
mani8338 <manib2500@gmail.com>
2025-04-13 23:54:27 +0330

    ComplexNums

---
+++ S14/HW/S14con.Test/Test1.cs
@@ -0,0 +1,22 @@
+﻿namespace ComplexNumberTest
+{
+    [TestClass]
+    public class ComplexNumberTests
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            var c1 = new ComplexNumber<int>(3,4);
+            var c2 = new ComplexNumber<int>(1,2);
+            var cSum = c1.Add(c2);
+            var cExpectedSum = new ComplexNumber<int>(4, 6);
+            Assert.AreEqual(cSum, cExpectedSum);
+        }
+        [TestMethod]
+        public void MagnitudeTest()
+        {
+            var c = new ComplexNumber<int>(3,4);
+            Assert.AreEqual(c.Magnitude,5); 
+        }
+    }
+}
